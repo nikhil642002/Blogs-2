f9a6bd7d43f72e188c9b0f327262ea3363be6512
Parth Chauhan <parthchauhan05@gmail.com>
2017-12-22 11:13:25 +0530

    Urls

--- trydjango19/posts/admin.py
+++ trydjango19/posts/admin.py
@@ -3,6 +3,9 @@ from .models import Post
 # Register your models here.
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ['title','updated', 'timestamp']
+    list_display_links = ['updated']
+    list_filter = ['updated', 'timestamp']
+    search_fields = ['title', 'content']
     class Meta:
         model = Post
 admin.site.register(Post, PostModelAdmin)
