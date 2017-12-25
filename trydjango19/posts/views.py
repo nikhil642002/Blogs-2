4f2f5f182ce89a2d85f63445e63e910efec1b59c
Parth Chauhan <parthchauhan05@gmail.com>
2017-12-25 19:18:21 +0530

    Query Set

--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Post
 # Create your views here.
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
@@ -9,8 +10,10 @@ def post_detail(request):
     return HttpResponse("<h1>Detail</h1>")
 
 def post_list(request):
+    queryset = Post.objects.all()
     context = {
-        "title" : 'list'
+        "title" : 'list',
+        'object_list' : queryset
     }
     return render(request, "index.html", context)
 
