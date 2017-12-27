4f5d71140c76c1be389dce29d0b99e4a8fb85cc2
Parth Chauhan <parthchauhan05@gmail.com>
2017-12-27 11:40:51 +0530

    Paging

--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 from .form import PostForm
 # Create your views here.
@@ -25,11 +26,20 @@ def post_detail(request, id=None):
     return render(request, "post_detail.html", context)
 
 def post_list(request):
-    queryset = Post.objects.all()
-
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 10)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        queryset = paginator.page(1)
+    except EmptyPage:
+        queryset = paginator.page(paginator.num_pages)
     context = {
         "title" : 'list',
-        'object_list' : queryset
+        'object_list' : queryset,
+        'page_request_var':page_request_var
     }
     return render(request, "post_list.html", context)
 
